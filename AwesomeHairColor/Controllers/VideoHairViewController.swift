f9227d57e730ae33f971ca536fbd8789cf2b8fa0
SLoban <sloban@ibagroup.eu>
2019-11-10 17:24:14 +0300

    added hairColorPredictor

--- AwesomeHairColor/Controllers/VideoHairViewController.swift
+++ AwesomeHairColor/Controllers/VideoHairViewController.swift
@@ -11,7 +11,9 @@ import AVKit
 import AVFoundation
 import Fritz
 
-class VideoHairViewController: UIViewController {
+class VideoHairViewController: UIViewController, HairColorPredictor {
+
+    var color: HairColor!
 
     var photoLibraryPicker: PhotoLibraryPicker?
     var videoPlayer: AVPlayer!
@@ -19,7 +21,7 @@ class VideoHairViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        color = HairColor(hairColor: UIColor.red)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -28,22 +30,6 @@ class VideoHairViewController: UIViewController {
         self.photoLibraryPicker = PhotoLibraryPicker(presentationController: self, delegate: self)
         self.photoLibraryPicker?.present(from: view)
     }
-    
-
-    func predict(with source: FritzVisionImage) -> UIImage? {
-        guard let result = try? visionModel.predict(source),
-            let mask = result.buildSingleClassMask(forClass: FritzVisionHairClass.hair)
-            else { return nil }
-
-        let blended = source.blend(
-            withMask: mask,
-            blendKernel: .softLight,
-            opacity: 0.7
-        )
-
-        return blended
-    }
-
 }
 
 extension VideoHairViewController: PhotoLibraryPickerDelegate {
